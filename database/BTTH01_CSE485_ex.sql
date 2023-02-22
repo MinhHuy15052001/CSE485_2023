5cb07b7ed90757b73d9c953bcf2d85528063d0ae
MinhHuy15052001 <122296804+MinhHuy15052001@users.noreply.github.com>
2023-02-22 22:58:41 +0700

    Câu I J

--- database/BTTH01_CSE485_ex.sql
+++ database/BTTH01_CSE485_ex.sql
@@ -1,8 +1,8 @@
--- Câu a
+-- Câu a Liệt kê các bài viết về các bài hát thuộc thể loại Nhạc trữ tình
 
 SELECT * FROM baiviet WHERE ma_tloai = (SELECT ma_tloai FROM theloai where ten_tloai = 'Nhạc trữ tình');
 
--- Câu b
+-- Câu b Liệt kê các bài viết của tác giả “Nhacvietplus”
 
 SELECT * FROM `baiviet` WHERE ma_tgia = (SELECT ma_tgia FROM tacgia where ten_tgia = 'Nhacvietplus')
 
@@ -34,3 +34,29 @@ GROUP BY ma_tloai
 ORDER BY COUNT(ma_tloai) DESC
 LIMIT 1)
 )
+
+
+-- Câu i Tạo 1 view có tên vw_Music để hiển thị thông tin về Danh sách các bài viết kèm theo Tên thể loại và tên tác giả
+CREATE VIEW vw_Music AS
+SELECT ma_bviet, tieude, ten_bhat, baiviet.ma_tloai, tomtat, noidung, baiviet.ma_tgia, ngayviet, hinhanh, ten_tgia, ten_tloai
+FROM tacgia, baiviet, theloai
+WHERE tacgia.ma_tgia = baiviet.ma_tgia AND baiviet.ma_tloai = theloai.ma_tloai
+
+SELECT * FROM vw_Music
+
+/* Câu j Tạo 1 thủ tục có tên sp_DSBaiViet với tham số truyền vào là Tên thể loại và trả về danh sách
+Bài viết của thể loại đó. Nếu thể loại không tồn tại thì hiển thị thông báo lỗi.*/
+CREATE PROCEDURE sp_DSBaiViet (IN input_ten_danhmuc VARCHAR(255))
+BEGIN
+DECLARE danhmuc_id INT;
+
+IF danhmuc_id IS NULL THEN
+SELECT 'Thể loại không tồn tại' AS message;
+ELSE
+
+SELECT * FROM baiviet WHERE ma_tloai = danhmuc_id;
+END IF;
+END
+
+--Thực thi thủ tục
+CALL sp_DSBaiViet('Tên thể loại cần tìm');
