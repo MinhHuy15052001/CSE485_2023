d6537e2873b1b9ebbf5e1b833c997a03730515ee
Minh Huy <102792495+minhhuy1505@users.noreply.github.com>
2023-02-22 14:54:46 +0700

    Giaodien

--- database/BTTH01_CSE485_ex.sql
+++ database/BTTH01_CSE485_ex.sql
@@ -4,7 +4,7 @@ SELECT * FROM baiviet WHERE ma_tloai = 2;
 
 -- Câu b
 
-SELECT * FROM `baiviet` WHERE ma_tgia = (SELECT  ma_tgia FROM tacgia where ten_tgia = 'Nhacvietplus')
+SELECT * FROM `baiviet` WHERE ma_tgia = (SELECT ma_tgia FROM tacgia where ten_tgia = 'Nhacvietplus')
 
 -- Câu c
 
